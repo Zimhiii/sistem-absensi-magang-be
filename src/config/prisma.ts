34c2cd32a6b5e798c58006851a7bc06706477fc0
Zimhiii <hijazmih@gmail.com>
2025-07-21 11:58:21 +0700

    testing

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -1,5 +1,14 @@
 import { PrismaClient } from "../../generated/prisma";
 
-const prisma = new PrismaClient();
+let prisma = new PrismaClient({ log: ["query", "info", "warn", "error"] });
+
+if (!globalThis.prisma) {
+  globalThis.prisma = new PrismaClient();
+}
+prisma = globalThis.prisma;
+
+declare global {
+  var prisma: PrismaClient;
+}
 
 export default prisma;
